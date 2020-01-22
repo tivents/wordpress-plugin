ac0e239ca1da1e424d8385252ed2bb9805525516
Willi Helwig <willi.helwig@cocase.eu>
2020-01-22 15:38:49 +0100

    add tests, reduce php versions

---
+++ tests/controllers/ProductsTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * ProductsTest.php Class Doc Comment
+ *
+ * @category Class
+ * @package  wordpress-plugin
+ * @author   whelwig
+ * @license  Restricted
+ * @link     https://cocase.eu/
+ *
+ */
+
+
+class ProductsTest extends PHPUnit_Framework_TestCase {
+
+	public function testSetProductTime() {
+		$this->assertTrue(true);
+	}
+
+	public function testGetProductUrl() {
+		$this->assertTrue(true);
+	}
+}
---
+++ tests/productsTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * productsTest.php Class Doc Comment
+ *
+ * @category Class
+ * @package  wordpress-plugin
+ * @author   whelwig
+ * @license  Restricted
+ * @link     https://cocase.eu/
+ *
+ */
+
+
+class productsTest extends PHPUnit_Framework_TestCase {
+
+	public function testSetProductTime() {
+		$this->assertTrue(true);
+	}
+}
